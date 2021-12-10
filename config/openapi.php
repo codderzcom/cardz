fefba6291c05281697083fc50c918779494957e9
Michael Altufiev <arutomi@gmail.com>
2021-12-10 11:49:49 +0300

    customer doc

---
+++ config/openapi.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'ags' => [
+        [
+            'name' => 'customer',
+            'description' => 'Pertains to customer operations',
+        ],
+
+        [
+            'name' => 'workspace',
+            'description' => 'Pertains to workspace operations',
+        ],
+
+    ],
+];
