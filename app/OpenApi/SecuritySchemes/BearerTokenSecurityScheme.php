1c2735fa72513b816955710fc711d180bd0e1d81
Michael Altufiev <arutomi@gmail.com>
2022-01-11 09:53:11 +0300

    Fix doc

--- app/OpenApi/SecuritySchemes/BearerTokenSecurityScheme.php
+++ app/OpenApi/SecuritySchemes/BearerTokenSecurityScheme.php
@@ -10,6 +10,7 @@ class BearerTokenSecurityScheme extends SecuritySchemeFactory
     public function build(): SecurityScheme
     {
         return SecurityScheme::create('BearerToken')
+            ->name('BearerToken')
             ->type(SecurityScheme::TYPE_API_KEY)
             ->in(SecurityScheme::IN_HEADER)
             ->scheme('bearer')
