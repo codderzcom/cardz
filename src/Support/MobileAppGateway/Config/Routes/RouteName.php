a16dacafb79acd32534878ea5e16e4ea0356bef8
Michael Altufiev <arutomi@gmail.com>
2022-01-12 16:54:01 +0300

    Fix names

--- src/Support/MobileAppGateway/Config/Routes/RouteName.php
+++ src/Support/MobileAppGateway/Config/Routes/RouteName.php
@@ -6,42 +6,42 @@ use MyCLabs\Enum\Enum;
 
 final class RouteName extends Enum
 {
-    public const REGISTER = 'CustomerRegister';
-    public const GET_TOKEN = 'CustomerGetToken';
-    public const GET_WORKSPACES = 'WorkspaceListAll';
-    public const ADD_WORKSPACE = 'WorkspaceAdd';
-    public const GET_PLANS = 'PlanListAll';
-    public const ISSUE_CARD = 'CardIssue';
-    public const GET_CARD = 'CardGet';
-    public const COMPLETE_CARD = 'CardComplete';
-    public const REVOKE_CARD = 'CardRevoke';
-    public const BLOCK_CARD = 'CardBlock';
-    public const UNBLOCK_CARD = 'CardUnblock';
+    public const REGISTER = 'RegisterCustomer';
+    public const GET_TOKEN = 'GetCustomerToken';
+    public const GET_WORKSPACES = 'ListAllWorkspace';
+    public const ADD_WORKSPACE = 'AddWorkspace';
+    public const GET_PLANS = 'ListAllPlan';
+    public const ISSUE_CARD = 'IssueCard';
+    public const GET_CARD = 'GetCard';
+    public const COMPLETE_CARD = 'CompleteCard';
+    public const REVOKE_CARD = 'RevokeCard';
+    public const BLOCK_CARD = 'BlockCard';
+    public const UNBLOCK_CARD = 'UnblockCard';
 
-    public const NOTE_ACHIEVEMENT = 'CardAchievementNote';
-    public const DISMISS_ACHIEVEMENT = 'CardAchievementDismiss';
+    public const NOTE_ACHIEVEMENT = 'NoteCardAchievement';
+    public const DISMISS_ACHIEVEMENT = 'DismissCardAchievement';
 
-    public const ADD_PLAN = 'PlanAdd';
-    public const CHANGE_PLAN_DESCRIPTION = 'PlanChangeDescription';
-    public const LAUNCH_PLAN = 'PlanLaunch';
-    public const STOP_PLAN = 'PlanStop';
-    public const ARCHIVE_PLAN = 'PlanArchive';
-    public const ADD_PLAN_REQUIREMENT = 'PlanRequirementAdd';
-    public const REMOVE_PLAN_REQUIREMENT = 'PlanRequirementRemove';
-    public const CHANGE_PLAN_REQUIREMENT = 'PlanRequirementChange';
-    public const PROPOSE_INVITE = 'InvitePropose';
-    public const ACCEPT_INVITE = 'InviteAccept';
-    public const DISCARD_INVITE = 'InviteDiscard';
+    public const ADD_PLAN = 'AddPlan';
+    public const CHANGE_PLAN_DESCRIPTION = 'ChangePlanDescription';
+    public const LAUNCH_PLAN = 'LaunchPlan';
+    public const STOP_PLAN = 'StopPlan';
+    public const ARCHIVE_PLAN = 'ArchivePlan';
+    public const ADD_PLAN_REQUIREMENT = 'AddPlanRequirement';
+    public const REMOVE_PLAN_REQUIREMENT = 'RemovePlanRequirement';
+    public const CHANGE_PLAN_REQUIREMENT = 'ChangePlanRequirement';
+    public const PROPOSE_INVITE = 'ProposeInvite';
+    public const ACCEPT_INVITE = 'AcceptInvite';
+    public const DISCARD_INVITE = 'DiscardInvite';
 
-    public const LEAVE_RELATION = 'CollaborationLeave';
-    public const FIRE_COLLABORATOR = 'CollaborationFire';
+    public const LEAVE_RELATION = 'LeaveWorkspace';
+    public const FIRE_COLLABORATOR = 'FireCollaborator';
 
-    public const GET_WORKSPACE = 'WorkspaceGet';
-    public const CHANGE_PROFILE = 'WorkspaceChangeProfile';
-    public const GET_PLAN = 'PlanGet';
+    public const GET_WORKSPACE = 'GetWorkspace';
+    public const CHANGE_PROFILE = 'ChangeWorkspaceProfile';
+    public const GET_PLAN = 'GetPlan';
 
-    public const CUSTOMER_WORKSPACES = 'CustomerWorkspacesGet';
-    public const CUSTOMER_ID = 'CustomerIdGet';
-    public const CUSTOMER_CARDS = 'CustomerCardsGet';
-    public const CUSTOMER_CARD = 'CustomerCardGet';
+    public const CUSTOMER_WORKSPACES = 'GetCustomerWorkspaces';
+    public const CUSTOMER_ID = 'GetCustomerId';
+    public const CUSTOMER_CARDS = 'GetCustomerCards';
+    public const CUSTOMER_CARD = 'GetCustomerCard';
 }
